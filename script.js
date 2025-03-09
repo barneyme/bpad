bb260bcbeb39f7cc0bfa7cd244cf95c6344018f3
barneyme <barney@barney.me>
2025-03-09 09:07:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,15 @@ var textbox = document.querySelector('#textbox');
 var timeoutID = null;
 var filenameBox = document.querySelector('#filename');
 
-// Automatically load/save cache in local storage when opening and closing the page
 textbox.value = localStorage.getItem('bpad') || '';
-textbox.setSelectionRange(textbox.value.length, textbox.value.length); // Place caret at end of content
-calcStats(); // Update counters after loading
+textbox.setSelectionRange(textbox.value.length, textbox.value.length); 
+calcStats(); 
 
 function storeLocally() { 
     localStorage.setItem('bpad', textbox.value); 
 }
-window.onbeforeunload = storeLocally; // Corrected event name
+window.onbeforeunload = storeLocally; 
 
-// Allow inputting tabs in the textarea instead of changing focus to the next element
 textbox.onkeydown = function (event) {
     if (event.key === "Tab") {
         event.preventDefault();
@@ -23,15 +21,12 @@ textbox.onkeydown = function (event) {
 };
 
 textbox.onkeyup = function () {
-    // Calculate text stats (using onkeyup is needed to update the count when deleting text)
     calcStats();
 
-    // Auto-save to local storage (at most once per second)
-    window.clearTimeout(timeoutID);
+        window.clearTimeout(timeoutID);
     timeoutID = window.setTimeout(storeLocally, 1000);
 };
 
-// Calculate and display character, words and line counts
 function calcStats() {
     updateCount('char', textbox.value.length);
     updateCount('word', textbox.value === "" ? 0 : textbox.value.replace(/\s+/g, ' ').split(' ').length);   
@@ -42,13 +37,11 @@ function updateCount(item, value) {
     document.querySelector('#' + item + '-count').textContent = value;
 }
 
-// Save textarea contents as a text file
 document.querySelector('#save a').onclick = function () {
     this.download = (filenameBox.value || 'bpad.txt').replace(/^([^.]*)$/, "$1.txt");
     this.href = URL.createObjectURL(new Blob([textbox.value], { type: 'text/plain' }));
 };
 
-// Load contents from a text file
 document.querySelector('#open a').onclick = function () {
     document.querySelector('#open input').click();
 };
